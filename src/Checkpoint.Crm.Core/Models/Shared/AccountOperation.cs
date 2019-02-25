98ea6df8ce540eab288b4cb6f442ae12e4296bcc
Sergey Zhizhenko <zlobnyiserg@gmail.com>
2019-02-25 15:26:43 +0300

    Актуализирован объект AccountOperation

--- src/Checkpoint.Crm.Core/Models/Shared/AccountOperation.cs
+++ src/Checkpoint.Crm.Core/Models/Shared/AccountOperation.cs
@@ -8,15 +8,72 @@ namespace Checkpoint.Crm.Core.Models.Shared
     /// </summary>
     public class AccountOperation : Model
     {
-        public string Name { get; set; }
-        public decimal Debit { get; set; }
-        public decimal Credit { get; set; }
-        public DateTime? ExpirationDate  { get; set; }
-        public bool Expireable { get; set; }
+        /// <summary>
+        /// Точка продаж, начислившая или списавшая баллы
+        /// </summary>
+        public int? PointOfSaleId { get; set; }
+        
+        /// <summary>
+        /// Целевой счёт
+        /// </summary>
         public int AccountId { get; set; }
+        
+        /// <summary>
+        /// Название операции (информационное поле)
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Сумма, если это начисление баллов
+        /// </summary>
+        public decimal Debit { get; set; }
+        
+        /// <summary>
+        /// Сумма, если это списание баллов
+        /// </summary>
+        public decimal Credit { get; set; }
+        
+        /// <summary>
+        /// Дата истечения срока действия баллов (если это начисление)
+        /// </summary>
+        public DateTime? ExpirationDate  { get; set; }
+        
+        /// <summary>
+        /// Номер заказа, благодаря которому баллы начисляются или в котором списываются в качестве оплаты
+        /// </summary>
         public int? OrderId { get; set; }
+        
+        /// <summary>
+        /// Номер правила вознаграждения, на основании которого баллы начислены
+        /// </summary>
         public int? RuleId { get; set; }
+        
+        /// <summary>
+        /// Тип бонусных баллов
+        /// </summary>
         public int? BonusTypeId { get; set; }
+        
+        /// <summary>
+        /// Пользователь-инициатор
+        /// </summary>
         public string InitiatorUser { get; set; }
+        
+        /// <summary>
+        /// Тип бонусных баллов (уровневые или платёжные)
+        /// </summary>
+        public BonusClass BonusClass { get; set; }
+    }
+
+    public enum BonusClass
+    {
+        /// <summary>
+        /// Уровневые баллы
+        /// </summary>
+        LVL,
+        
+        /// <summary>
+        /// Баллы для оплаты
+        /// </summary>
+        PAYM
     }
 }
