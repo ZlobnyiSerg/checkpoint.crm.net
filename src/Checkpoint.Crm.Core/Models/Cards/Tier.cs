7cd1fb71fb80b7ddd3ac4bc03852dfafed6e3ace
ZlobnyiSerg <zlobnyiserg@gmail.com>
2021-01-21 15:40:43 +0300

    Расширены поля справочника уровней (Tier)

--- src/Checkpoint.Crm.Core/Models/Cards/Tier.cs
+++ src/Checkpoint.Crm.Core/Models/Cards/Tier.cs
@@ -2,7 +2,23 @@
 
 namespace Checkpoint.Crm.Core.Models.Cards
 {
+    /// <summary>
+    /// Уровень гостя в системе лояльности
+    /// </summary>
     public class Tier : DictionaryEntityBaseModel
     {        
+        /// <summary>
+        /// Минимальная сумма уровневых баллов, необходимых для достижения данного уровня
+        /// </summary>
+        public int MinPoints { get; set; }
+        /// <summary>
+        /// Общее описание уровня
+        /// </summary>
+        public string Description { get; set; }
+        
+        /// <summary>
+        /// Список привилегий и возможностей уровня
+        /// </summary>
+        public string[] Features { get; set; }
     }
 }
--- src/Checkpoint.Crm.Core/Models/Shared/Account.cs
+++ src/Checkpoint.Crm.Core/Models/Shared/Account.cs
@@ -2,25 +2,28 @@ using Checkpoint.Crm.Core.Models.Base;
 
 namespace Checkpoint.Crm.Core.Models.Shared
 {
+    /// <summary>
+    /// Балльный счёт гостя
+    /// </summary>
     public class Account : Model
     {
         /// <summary>
-        /// Баланс
+        /// Баланс платёжных баллов
         /// </summary>
         public decimal Balance { get; set; }            
         
         /// <summary>
-        /// Дебет баланса
+        /// Дебет баланса (поступления платёжных баллов)
         /// </summary>
         public decimal BalanceDebit { get; set; }
         
         /// <summary>
-        /// Кредит баланса
+        /// Кредит баланса (расходы платёжных баллов)
         /// </summary>
         public decimal BalanceCredit { get; set; }
         
         /// <summary>
-        /// Сумма баллов с ограниченным сроком действия
+        /// Сумма платёжных баллов с ограниченным сроком действия
         /// </summary>
         public decimal BalanceExpirable { get; set; }  
         
